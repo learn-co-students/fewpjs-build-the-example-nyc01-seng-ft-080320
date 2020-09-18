166121008ce41a0bb32b76b0aa0d2a99a5f0db5d
royFloresNyc <royfloresnyc@gmail.com>
2020-09-17 21:39:17 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const body = document.querySelector('body')
+body.addEventListener('click', e => {
+    console.log(e.target)
+    const clicked = e.target
+    if (clicked.matches("span.like-glyph")){
+        if(clicked.textContent === FULL_HEART){
+            clicked.textContent = EMPTY_HEART
+            clicked.classList.remove('activated-heart')
+        } else {
+            mimicServerCall("someURL")
+            .then(response => {
+                clicked.textContent = FULL_HEART
+                clicked.classList.add('activated-heart')
+            })
+            .catch(error => {
+                const errorModal = document.querySelector('div#modal')
+                const errorMessage = errorModal.querySelector('p#modal-message')
+                errorMessage.textContent = error
+                errorModal.classList.remove('hidden')
+                setTimeout(()=>{
+                    errorModal.classList.add('hidden')
+                }, 5000)
+            })
+        }
+    }
+})
 
 
 
